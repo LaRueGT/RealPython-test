5e534f9f08a9f7bebde5e0b1b4a7573785d4d443
Garrett LaRue <gtlarue@gmail.com>
2016-02-12 15:49:18 -0500

    Initial commit

--- RealPython.py
+++ RealPython.py
@@ -1,12 +1,17 @@
+#import the Flask class
 from flask import Flask
 
+#create the application object
 app = Flask(__name__)
 
-
+#decorators to link function to URL
 @app.route('/')
+@app.route("/hello")
+#define the view using a function, which returns a string
 def hello_world():
     return 'Hello World!'
 
 
+#start dev server using the run() method
 if __name__ == '__main__':
     app.run()

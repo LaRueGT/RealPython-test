591ff6b9a4b71c92613db24f0e75e4428e331eca
Garrett LaRue <gtlarue@gmail.com>
2016-02-13 16:45:41 -0500

    SQL databases - selecting

--- sql/sqlb.py
+++ sql/sqlb.py
@@ -1,24 +1,28 @@
-# INSERT COMMAND
+# INSERT COMMAND with Error Handler
 
 # import the sqlite3 library
 import sqlite3
 
 # create the connection object
-with sqlite3.connect("new.db") as connection
+with sqlite3.connect("new.db") as connection:
 
-# get a cursor object used to execute sql commands
-cursor = connection.cursor()
+    # get a cursor object used to execute sql commands
+    cursor = connection.cursor()
 
-# insert data
-cursor.execute("INSERT INTO population  VALUES('New York City', \
-                'NY', 8200000)")
-cursor.execute("INSERT INTO population VALUES('San Francisco', \
-                'CA', 800000)")
+    try:
+        # insert data
+        cursor.execute("INSERT INTO populations  VALUES('New York City', \
+                        'NY', 8200000)")
+        cursor.execute("INSERT INTO populations VALUES('San Francisco', \
+                        'CA', 800000)")
+    except sqlite3.OperationalError:
+        print("something went wrong, crap")
+
 
 # unnecessary when using with..as clause
 # commit changes
-#conn.commit()
+# conn.commit()
 #
 # close the database connection
-#conn.close()
+# conn.close()
 
